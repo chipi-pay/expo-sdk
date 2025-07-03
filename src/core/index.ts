e5aad2773996331e50dd8dbb8aaafe121fb184a1
robertoyamanaka <ryamanakc@gmail.com>
2025-07-03 22:44:59 +0700

    fixed version

--- src/core/index.ts
+++ src/core/index.ts
@@ -2,4 +2,5 @@
 export { ChipiSDK } from './chipi-sdk';
 export { createArgentWallet } from './create-argent-wallet';
 export { executePaymasterTransaction } from './send-transaction-with-paymaster';
+export { BACKEND_URL } from './backend-url';
 export * from './types';
